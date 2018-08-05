3ff258ac481e08169b7accd79953b99e5991c8ef
osnth01 <osnth01@gmail.com>
2018-08-04 22:11:47 -0400

    create initial html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: path.join(__dirname, 'src', index.tsx),
+  entry: path.join(__dirname, 'src', 'index.tsx'),
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 3000
